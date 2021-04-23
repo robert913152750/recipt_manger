81285362ced1d5d676ded73194ff0ad9601039df
Robert <robert913152750@gmail.com>
2021-04-23 20:38:23 +0800

    can upload the file

--- services/receiptService.js
+++ services/receiptService.js
@@ -8,10 +8,16 @@ const User = db.User
 const receiptService = {
   async uploadReceipt (req, res, callback) {
     try {
-
+      const { file } = req
+      fs.readFile(file.path, (err, data) => {
+        const receipt = data.toString()
+        return callback({
+          receipt: receipt
+        })
+      })
     } catch (err) {
       console.log(err)
-      callback({
+      return callback({
         status: 'error',
         message: '上傳失敗'
       })
