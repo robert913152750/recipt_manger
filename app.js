9d6700aa2b3f56673aa2ea49c46763c75a4d5b53
Robert <robert913152750@gmail.com>
2021-04-26 10:32:21 +0800

    Install pg and update production config

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const port = 3000
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
